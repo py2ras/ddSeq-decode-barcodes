e4cb6f8d6156a27d85a7785146356de201f975c9
py2ras <sarthak.gatech@gmail.com>
2018-10-04 10:35:17 -0400

    Unit tests

---
+++ v0.1.1/tests/test_decoder.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+
+# Tests for decoder
+# @author: Sarthak Sharma <sarthaksharma@gatech.edu>
+# Date of Last modification - 10/01/2018
+
+import pytest
+import sys
+
+def test_getAllBlocks():
+    pass
+
+def test_findLinkerIndices():
+    pass
+
+def test_findAllOtherIndices():
+    pass
+
